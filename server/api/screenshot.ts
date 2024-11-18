6cace15e45eaa614c02ce8470c4b5474a3fa24d0
eiskaltDEV <atlasyigitaydin@gmail.com>
2024-11-18 17:25:55 +0300

    sparticuz

--- server/api/screenshot.ts
+++ server/api/screenshot.ts
@@ -1,12 +1,11 @@
-import chromium from 'chrome-aws-lambda'
+import chromium from '@sparticuz/chromium'
 import puppeteer from 'puppeteer-core'
-
 import { RateLimiterMemory } from 'rate-limiter-flexible'
 
 export default defineEventHandler(async (event) => {
   const rateLimiter = new RateLimiterMemory({
-    points: 5,
-    duration: 60,
+    points: 5, // Her IP'ye 5 istek limiti
+    duration: 60, // 60 saniyede 5 istek
   })
 
   const runtimeConfig = useRuntimeConfig()
@@ -28,15 +27,19 @@ export default defineEventHandler(async (event) => {
       headers: { 'Content-Type': 'application/json' },
     })
   }
+
+  // URL ve parametreler
   const query = getQuery(event)
   const url = query.url as string
   const width = Number.parseInt(query.width as string) || 1920
   const deviceScaleFactor = Number.parseFloat(query.deviceScaleFactor as string) || 1
 
+  // IP adresi alımı
   const ip = Array.isArray(event.node.req.headers['x-forwarded-for'])
     ? event.node.req.headers['x-forwarded-for'][0]
     : event.node.req.headers['x-forwarded-for'] || event.node.req.socket.remoteAddress
 
+  // IP doğrulama
   if (!ip || typeof ip !== 'string') {
     return {
       statusCode: 400,
@@ -44,6 +47,7 @@ export default defineEventHandler(async (event) => {
     }
   }
 
+  // URL kontrolü
   if (!url) {
     return {
       statusCode: 400,
@@ -51,34 +55,41 @@ export default defineEventHandler(async (event) => {
     }
   }
 
+  // İstek sınırını kontrol et
   try {
-    await rateLimiter.consume(ip)
-      .catch((error) => {
-        return {
-          statusCode: 429,
-          message: `İstek sınırı aşıldı - ${error}`,
-        }
-      })
+    await rateLimiter.consume(ip) // IP bazlı sınırlama
+  }
+  catch (error) {
+    return {
+      statusCode: 429,
+      message: `İstek sınırı aşıldı - ${error}`,
+    }
+  }
 
+  // Puppeteer ile ekran görüntüsü alma
+  try {
     const browser = await puppeteer.launch({
       ignoreDefaultArgs: ['--disable-extensions'],
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-      defaultViewport: chromium.defaultViewport,
+      args: chromium.args, // Lambda'ya uygun args
+      executablePath: await chromium.executablePath(), // Burada await kullanarak asenkron fonksiyonu doğru şekilde çağırıyoruz
+      headless: chromium.headless, // Başsız modda çalışacak
+      defaultViewport: chromium.defaultViewport, // Varsayılan viewport
     })
 
     const page = await browser.newPage()
 
+    // Sayfa ayarları
     await page.setViewport({
       width,
       height: 1080,
       deviceScaleFactor,
     })
 
-    await page.goto(url, { waitUntil: 'networkidle2'/* , timeout: 60000 */ })
+    // Sayfayı ziyaret et
+    await page.goto(url, { waitUntil: 'networkidle2' })
     await page.waitForSelector('body')
 
+    // Ekran görüntüsünü al
     const screenshot = await page.screenshot({
       encoding: 'base64',
       fullPage: true,
@@ -86,6 +97,7 @@ export default defineEventHandler(async (event) => {
 
     await browser.close()
 
+    // Telegram'a bildirim gönder
     await sendToTelegram(url)
 
     return {
