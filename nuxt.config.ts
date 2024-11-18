76712545316c6d1e081fa6722194af0177b4c060
eiskaltDEV <atlasyigitaydin@gmail.com>
2024-11-18 13:30:15 +0300

    change public

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,18 +8,18 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       firebase: {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-        documentConfig: process.env.FIREBASE_DOCUMENT_CONFIG,
+        apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+        measurementId: process.env.NUXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+        documentConfig: process.env.NUXT_PUBLIC_FIREBASE_DOCUMENT_CONFIG,
       },
       telegram: {
-        botToken: process.env.TELEGRAM_BOT_TOKEN,
-        chatId: process.env.TELEGRAM_CHAT_ID,
+        botToken: process.env.NUXT_PUBLIC_TELEGRAM_BOT_TOKEN,
+        chatId: process.env.NUXT_PUBLIC_TELEGRAM_CHAT_ID,
       },
     },
   },
