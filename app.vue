e7ae98c199ccd555dfc6adb488a73af10654e53f
eiskaltDEV <atlasyigitaydin@gmail.com>
2024-11-18 18:29:24 +0300

    system error

--- app.vue
+++ app.vue
@@ -59,6 +59,11 @@ useHead({
 </script>
 
 <template>
+  <div class="fixed top-12 left-0 w-full text-white animate-pulse z-50 h-12 flex justify-center items-center">
+    <div class="bg-red-600 font-bold rounded px-3 py-2">
+      SYSTEM ERROR!
+    </div>
+  </div>
   <Loading :loading="screenShotStore.loading" />
   <NuxtPage />
 </template>
