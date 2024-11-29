b8adc23693f0c3529607b2488ce183b2e7fc8f91
eiskaltDEV <atlasyigitaydin@gmail.com>
2024-11-29 13:40:06 +0300

    first function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,7 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { logger } from 'firebase-functions'
+import { onRequest } from 'firebase-functions/v2/https'
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = onRequest((request, response) => {
+  logger.info('Hello logs!', { structuredData: true })
+  response.send('Hello from Firebase!')
+})
