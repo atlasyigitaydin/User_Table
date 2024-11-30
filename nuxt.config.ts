aaa2468be9d99943ba5fa4c5480beb7934e30f35
Atlas <atlasyigitaydin@gmail.com>
2024-11-30 16:20:35 +0300

    add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Web Screenshot',
+    },
+  },
   ssr: true,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
